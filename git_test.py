d804d444e736052b0860bfe6150a1e78a1199d59
Angel Mendez <angelmendez074@gmail.com>
2019-12-22 01:35:13 -0500

    Version 4.2.0

--- git_test.py
+++ git_test.py
@@ -5,6 +5,9 @@ Created on Sun Dec 22 01:19:09 2019
 @author: angel
 """
 #git test
-
-for(i in range 2000): #meaningless
-    print("10100001") 
+#now we testing another commit
+for(i in range 2000): # still meaningless
+    if(i % 2 == 0):
+        print("10100001") 
+    else:
+        print("bump")
