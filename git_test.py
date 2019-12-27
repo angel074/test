9aedc3b26c9e3a2d546710b9bc989566bb28a7e6
Angel Mendez <angelmendez074@gmail.com>
2019-12-26 19:18:46 -0500

    Revert "Version 4.2.0"
    This reverts commit d804d444e736052b0860bfe6150a1e78a1199d59.

--- git_test.py
+++ git_test.py
@@ -5,9 +5,6 @@ Created on Sun Dec 22 01:19:09 2019
 @author: angel
 """
 #git test
-#now we testing another commit
-for(i in range 2000): # still meaningless
-    if(i % 2 == 0):
-        print("10100001") 
-    else:
-        print("bump")
+
+for(i in range 2000): #meaningless
+    print("10100001") 
